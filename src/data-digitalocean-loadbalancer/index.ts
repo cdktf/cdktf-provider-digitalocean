f96a85057aacdb32674498eba45baae87a6e1435
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-02-05 10:26:49 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/data-digitalocean-loadbalancer/index.ts
+++ src/data-digitalocean-loadbalancer/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/digitalocean/digitalocean/2.34.1/docs/data-sources/loadbalancer
 // generated from terraform resource schema
 
