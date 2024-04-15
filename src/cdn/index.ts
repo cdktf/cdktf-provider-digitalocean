6643746f2357122cd05d04675fea05fbbe29f00d
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-04-15 07:10:30 +0000

    chore(deps): update  by cdktf/cdktf-repository-manager@0754ac0

--- src/cdn/index.ts
+++ src/cdn/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/digitalocean/digitalocean/2.36.0/docs/resources/cdn
 // generated from terraform resource schema
 
