7b772651c8fb53de153f34065622f109bad02e37
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-05-27 08:46:37 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/uptime-alert/index.ts
+++ src/uptime-alert/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/digitalocean/digitalocean/2.38.0/docs/resources/uptime_alert
 // generated from terraform resource schema
 
