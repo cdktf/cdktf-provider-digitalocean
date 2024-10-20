6cf091a9a31429033d61f1289884cd60a1b6a11d
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-10-20 21:54:39 +0000

    chore(deps): update  by cdktf/cdktf-repository-manager@c89e53a

--- src/container-registry-docker-credentials/index.ts
+++ src/container-registry-docker-credentials/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/digitalocean/digitalocean/2.43.0/docs/resources/container_registry_docker_credentials
 // generated from terraform resource schema
 
