6859a9150e0a041f36cdc2da275b129736bdd310
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-12-15 05:57:53 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/data-digitalocean-reserved-ipv6/index.ts
+++ src/data-digitalocean-reserved-ipv6/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/digitalocean/digitalocean/2.46.0/docs/data-sources/reserved_ipv6
 // generated from terraform resource schema
 
