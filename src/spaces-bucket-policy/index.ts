f75f61b5e7a5966dc83d7aaa93ff87b56393959e
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2025-03-02 05:56:56 +0000

    chore(deps): update  by cdktf/cdktf-repository-manager@b33609d

--- src/spaces-bucket-policy/index.ts
+++ src/spaces-bucket-policy/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/digitalocean/digitalocean/2.49.1/docs/resources/spaces_bucket_policy
 // generated from terraform resource schema
 
